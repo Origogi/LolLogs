d463894d7111eefd3dddcc44537add73eb2f4350
origogi <rlawjdxo1212@naver.com>
2021-12-10 11:40:53 +0900

    first commit

---
+++ app/src/main/java/com/origogi/lollogs/view/SearchResultView.kt
@@ -0,0 +1,12 @@
+package com.origogi.lollogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.origogi.lollogs.R
+
+class SearchResultView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result_view)
+    }
+}
